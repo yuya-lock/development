3c7b12d4302b2ea76667b352705301752cc90806
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2021-12-11 17:41:11 +0900

    seedデータの修正

--- db/seeds/development/users.rb
+++ db/seeds/development/users.rb
@@ -4,7 +4,7 @@ fnames_mail = [ "sato", "suzuki", "takahashi", "tanaka"]
 gnames_mail = [ "taro", "ziro", "hanako"]
 10.times do |i|
   User.create(
-    name: "#{fnames[i % 4]} #{gnames[i % 3]}",
+    name: "#{fnames[i % 4]}#{gnames[i % 3]}",
     mail: "#{fnames_mail[i % 4]}#{gnames_mail[i % 3]}}@example.com",
     password: "example"
   )
