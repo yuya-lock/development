c8413a98eb58d160464a5dc07c7095962bf61a04
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2021-12-11 20:23:16 +0900

    usersテーブルのis_hostカラムの削除

--- db/migrate/20211211065633_create_users.rb
+++ db/migrate/20211211065633_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :name, null: false # 名前
       t.string :mail, null: false # メールアドレス
       t.string :password, null: false # パスワード
-      t.boolean :is_host, default: false # チケット作成者のチケット管理権限
       t.string :top_image # トップ画像
       t.string :back_image # 背景画像
 
