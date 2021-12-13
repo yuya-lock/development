85e83ed2d159a58914fef972fbc6298d81321abe
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2021-12-13 18:56:49 +0900

    セッションを用いたログイン機能の導入

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root "top#index"
   get "about" => "top#about", as: "about"
   get "content" => "top#content", as: "content"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
 
   resources :users, except: [:index]
 end
