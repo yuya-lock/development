299f645d3584f15272cabe218317791a52a64f92
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2021-12-14 15:10:43 +0900

    マイグレーションスクリプトをきちんと作成できていなかったため修正

--- db/migrate/20211214013030_add_remember_digest_to_users.rb
+++ db/migrate/20211214013030_add_remember_digest_to_users.rb
@@ -1,4 +1,4 @@
-class  AddRememberDigestToUsers < ActiveRecord::Migration[6.1]
+class AddRememberDigestToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :remember_digest, :string
   end
