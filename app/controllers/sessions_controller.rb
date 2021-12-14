b9f9f98401ec59400e2cb74d6bcbc455092b451a
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2021-12-14 21:33:42 +0900

    createアクションを少し変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(mail: params[:session][:mail].downcase)
-    if user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
