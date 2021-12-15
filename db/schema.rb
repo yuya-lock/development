f3324a847187d6978a0b8f4f1a27c5b419f175fb
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2021-12-15 19:44:48 +0900

    テーブルの変更を反映

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,15 @@
 ActiveRecord::Schema.define(version: 2021_12_15_023201) do
 
   create_table "posts", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "circle_name", null: false
     t.string "university", null: false
     t.text "body", null: false
+    t.datetime "posted_at", null: false
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -33,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_12_15_023201) do
     t.index ["mail"], name: "index_users_on_mail", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
