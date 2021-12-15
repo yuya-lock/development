0a90dd013f3d51de8da85f0b94dd35953604e3ef
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2021-12-15 12:43:42 +0900

    バリデーション、シードデータの作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :circle_name, :university, :body, presence: true
+  validates :circle_name, :university, length: { maximum: 30 }
+  validates :body, length: { maximum: 2000 }
 end
