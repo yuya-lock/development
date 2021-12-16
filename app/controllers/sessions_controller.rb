50dd3e3c772b615c0b4f7b28d3b49c30a54c5915
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2021-12-17 07:55:40 +0900

    ログイン後トップページに画面遷移するように変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_to :root
     else
       flash.now[:danger] = "名前とパスワードが一致しません"
       render "new"
