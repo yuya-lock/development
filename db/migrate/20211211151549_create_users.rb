06c503ce12892bfdc3b4e06b25dca96744e40ccb
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2021-12-18 21:05:40 +0900

    カラムの削除

--- db/migrate/20211211151549_create_users.rb
+++ db/migrate/20211211151549_create_users.rb
@@ -4,8 +4,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :name, null: false # ユーザー名
       t.string :mail, null: false # メールアドレス
       t.string :password_digest, null: false # パスワード
-      t.string :top_image # トップ画像
-      t.string :back_image # 背景画像
 
       t.timestamps
     end
