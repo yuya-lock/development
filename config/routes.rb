d6649af98213876657e4bf069cbd41342f3a6cf2
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2021-12-20 10:15:42 +0900

    段落分けを行う

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get "login" => "sessions#new"
   post "login" => "sessions#create"
   delete "logout" => "sessions#destroy"
+
+  # エラー処理
   get "bad_request" => "top#bad_request"
   get "forbidden" => "top#forbidden"
   get "internal_server_error" => "top#internal_server_error"
@@ -12,6 +14,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show] do
     resources :posts, only: [:index]
   end
+
   resource :account, only: [:show, :edit, :update, :destroy]
   resource :password, only: [:show, :edit, :update]
   resources :posts
