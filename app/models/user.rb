f1c868ffe1e5b69efd4c1dd529986bd367306b6d
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2021-12-20 11:05:39 +0900

    行整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
-  before_save { self.mail = mail.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :mail, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },  uniqueness: { case_sensitive: false }, on: :create
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, on: :create
-
   attr_accessor :current_password
+
+  # ↓バリデーション↓
+  validates :name, presence: true, length: { maximum: 50 }
+  
+  before_save { self.mail = mail.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :mail, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }, on: :create
+  has_secure_password
+  
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
   validates :password, presence: { if: :current_password }
+  # ↑バリデーション↑
 
   has_many :posts, dependent: :destroy
 
