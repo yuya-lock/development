d5012c586c6e5baf1ec50ca065ede9d79bbc8dbb
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2021-12-20 10:56:57 +0900

    列整理

--- db/migrate/20211211151549_create_users.rb
+++ db/migrate/20211211151549_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name, null: false # ユーザー名
-      t.string :mail, null: false # メールアドレス
-      t.string :password_digest, null: false # パスワード
+      t.string :name, null: false             # ユーザー名
+      t.string :mail, null: false             # メールアドレス
+      t.string :password_digest, null: false  # パスワード
 
       t.timestamps
     end
