6e3fb3a16aa5218d3dd58e93058f6524bcd19e02
yuyahappy1009@gmail.com <yuyahappy1009@suou.waseda.jp>
2022-01-01 13:52:42 +0900

    JavaScriptが実行可能なドライバを設定

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,2 +1,9 @@
-require 'capybara/poltergeist'
-Capybara.javascript_driver = :selenium_chrome_headless
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
+end
